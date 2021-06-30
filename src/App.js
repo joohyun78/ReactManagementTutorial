05f8cb8a4961ac33b924bdac1279404ee526d51f
joohyun <nonsuch78@gmail.com>
2021-06-30 16:14:36 +0900

    master

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React000000000000000
         </a>
       </header>
     </div>
